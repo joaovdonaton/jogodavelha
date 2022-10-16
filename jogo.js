1fde99e07563355e8f8584ad26b3bd1d4e9045ba
joaovdonaton <joao.vitor.donaton@gmail.com>
2022-10-16 15:00:28 -0300

    sistema básico para jogar

--- jogo.js
+++ jogo.js
@@ -1,16 +1,40 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 function imprimirTabuleiro(tabuleiro){
     //FAZER - GENERALIZAR ESSA FUNÇÃO
     console.log('-------------')
     tabuleiro.forEach((linha, i, a) => {
         process.stdout.write('|')
         linha.forEach(e => {
-            process.stdout.write(` ${e} ` +'|'); //imprimir sem dar newline
+            process.stdout.write(` ${e} ` +'|');
         })
         console.log( i === a.length-1 ? '\n-------------': '\n|---|---|---|')
     })
 }
 
-const tabuleiro = []
+function jogarTabuleiro(linha, coluna, char, tabuleiro){
+    const result = []
+    //copiar tabuleiro
+    for (const e of tabuleiro) {
+        result.push([...e])
+    }
+
+    result[linha][coluna] = char.toUpperCase();
+    return result
+}
+
+function input(texto=''){
+    return new Promise((resolve, reject) => {
+        readline.question(texto, (i) => {
+            resolve(i)
+        })
+    })
+}
+
+let tabuleiro = []
 
 //gerar tabuleiro
 for (let i = 0; i < 3; i++) {
@@ -21,3 +45,14 @@ for (let i = 0; i < 3; i++) {
 }
 
 imprimirTabuleiro(tabuleiro)
+
+async function pergunta(){
+    const pos1 = await input('#O Qual posição deseja jogar (Linha Coluna)? ')
+    tabuleiro = jogarTabuleiro(...pos1.split(' '), 'O', tabuleiro)
+    const pos2 = await input('#X Qual posição deseja jogar (Linha Coluna)? ')
+    tabuleiro = jogarTabuleiro(...pos2.split(' '), 'X', tabuleiro)
+
+    imprimirTabuleiro(tabuleiro)
+}
+
+pergunta()
