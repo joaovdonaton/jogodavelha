580b2d96185e3088c52272d95761af47003c11b5
joaovdonaton <joao.vito.donaton@gmail.com>
2022-11-04 21:28:10 -0300

    jogo html funcional

--- script.mjs
+++ script.mjs
@@ -21,42 +21,82 @@ for (let i = 0; i < 3; i++) {
         // fazer jogada
         e.addEventListener('click', (event) => {
             //jogada com sucesso
-            if(tabuleiro.jogar(parseInt(event.target.getAttribute('linha')),
+            if (tabuleiro.jogar(parseInt(event.target.getAttribute('linha')),
                 parseInt(event.target.getAttribute('coluna')),
                 jogadorAtual)) {
                 tabuleiro.renderizar(tabuleiroElement)
 
-                const vitoria = tabuleiro.verificarVitoria()
-                if(vitoria){
-                    alert('Jogador ' + vitoria + ' venceu!')
-                }
-
                 trocarJogador()
 
                 //jogada do bot
-                if(jogadorAtual === 'O'){
+                if (jogadorAtual === 'O' && modo !== 'jogador') {
+                    const jogadasDisp = tabuleiro.jogadasDisponiveis()
 
+                    if (modo === 'easy') {
+                        const [l, c] = jogadasDisp[Math.floor(Math.random() * jogadasDisp.length)]
+
+                        tabuleiro.jogar(l, c, jogadorAtual)
+                    }
+                    /*
+                    * faz uma jogada se ela for vencer pro bot, caso contrário, jogada aleatória (ou bloquear jogada no hard)
+                    * */
+                    else if (modo === 'normal' || modo === 'hard') {
+                        let venci = false;
+                        for (const [l, c] of jogadasDisp) {
+                            if (tabuleiro.simularJogada(l, c, jogadorAtual)) {
+                                tabuleiro.jogar(l, c, jogadorAtual)
+                                venci = true
+                                break
+                            }
+                        }
+
+                        let bloqueado = false;
+                        if (!venci) {
+                            if (modo === 'hard') { //bloquear jogada do outro
+                                for (const [l, c] of jogadasDisp) {
+                                    if (tabuleiro.simularJogada(l, c, jogadorAtual === 'O' ? 'X' : 'O', tabuleiro)) {
+                                        tabuleiro.jogar(l, c, jogadorAtual, tabuleiro)
+                                        bloqueado = true;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+
+                        if (!venci && !bloqueado) {
+                            const [l, c] = jogadasDisp[Math.floor(Math.random() * jogadasDisp.length)]
+
+                            tabuleiro.jogar(l, c, jogadorAtual)
+                        }
+                    }
+                    tabuleiro.renderizar(tabuleiroElement)
+                    trocarJogador()
+                }
+
+                const vitoria = tabuleiro.verificarVitoria()
+                if (vitoria) {
+                    alert('Jogador ' + vitoria + ' venceu!')
                 }
             }
-            else{
+            else {
                 alert('Jogada Impossível!')
             }
         })
+
         tabuleiroElement.appendChild(e)
     }
 }
-
 // seleção do modo de jogo
-const menuButtons = document.querySelectorAll(".menuButton")
-menuButtons.forEach(e => e.addEventListener('click', (e) => {
-    modo = e.target.getAttribute('value')
-    document.querySelector('#menu').style.visibility = 'hidden'
-    tabuleiroElement.style.visibility = 'visible';
+    const menuButtons = document.querySelectorAll(".menuButton")
+    menuButtons.forEach(e => e.addEventListener('click', (e) => {
+        modo = e.target.getAttribute('value')
+        document.querySelector('#menu').style.visibility = 'hidden'
+        tabuleiroElement.style.visibility = 'visible';
 
-    if(modo === 'jogador'){
-        document.querySelector('#vez').style.visibility = 'visible'
-    }
+        if (modo === 'jogador') {
+            document.querySelector('#vez').style.visibility = 'visible'
+        }
 
-    tabuleiro.renderizar(tabuleiroElement)
-}))
+        tabuleiro.renderizar(tabuleiroElement)
+    }))
 
